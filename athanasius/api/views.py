662e7815ff3fbbdb40bb5b146b02564fc0bf3b9b
Giorgio Caviglia <giorgio.caviglia@gmail.com>
2012-10-12 00:20:31 +0200

    fixing upload... again

--- athanasius/api/views.py
+++ athanasius/api/views.py
@@ -171,11 +171,15 @@ def api_upload(request):
             
             new_file = open(os.path.join(django_settings.BASE_PATH, 'tmp/tmp.txt'), 'r')
             
+            reader = csv.DictReader( new_file.read(), dialect=csv.excel_tab )
+            
+            
+            """
             lines = []
             for line in new_file:
                 lines.append(line)#.decode(encoding).encode('utf-8')
             
-            """
+            
             encoding = chardet.detect(lines[0])
             encoding = encoding['encoding']
             
@@ -211,7 +215,7 @@ def api_upload(request):
             dialect = sniffer.sniff(f.read())
             dialect.delimiter = "\t"
             """
-            response['result']['problema'] = len(lines)
+            response['result']['problema'] = len(reader)
         
         except Exception, e:
             response = createResponse401(str(e))
